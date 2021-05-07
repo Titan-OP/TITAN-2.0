90cf0f002771d9ebbc3939fe0052eb1f679f8145
Titan-OP <83203983+Titan-OP@users.noreply.github.com>
2021-05-07 14:03:55 +0530

    Update admin.py

--- TITAN/Modules/admin.py
+++ TITAN/Modules/admin.py
@@ -499,7 +499,7 @@ dispatcher.add_handler(DEMOTE_HANDLER)
 dispatcher.add_handler(SET_TITLE_HANDLER)
 dispatcher.add_handler(ADMIN_REFRESH_HANDLER)
 
-__mod_name__ = "Admin 🎖"
+__mod_name__ = "Admin 👮"
 __command_list__ = [
     "adminlist",
     "admins",
