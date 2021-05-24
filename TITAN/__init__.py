0104eafb29125a1da47d82baaec9270c2e2ea940
Krishna <72682463+krishna6688@users.noreply.github.com>
2021-05-24 09:37:28 +0530

    Update __init__.py

--- TITAN/__init__.py
+++ TITAN/__init__.py
@@ -191,7 +191,7 @@ DEMONS = list(DEMONS)
 TIGERS = list(TIGERS)
 
 # Load at end to ensure all prev variables have been set
-from SaitamaRobot.modules.helper_funcs.handlers import (
+from TITAN.modules.helper_funcs.handlers import (
     CustomCommandHandler,
     CustomMessageHandler,
     CustomRegexHandler,
