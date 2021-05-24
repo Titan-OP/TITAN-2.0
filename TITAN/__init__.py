d82e6be02d7c32981e25ab4f9498bf9f619abcc1
Krishna <72682463+krishna6688@users.noreply.github.com>
2021-05-24 09:54:56 +0530

    Update __init__.py

--- TITAN/__init__.py
+++ TITAN/__init__.py
@@ -191,7 +191,7 @@ DEMONS = list(DEMONS)
 TIGERS = list(TIGERS)
 
 # Load at end to ensure all prev variables have been set
-from titan.modules.helper_funcs.handlers import (
+from TITAN.modules.helper_funcs.handlers import (
     CustomCommandHandler,
     CustomMessageHandler,
     CustomRegexHandler,
